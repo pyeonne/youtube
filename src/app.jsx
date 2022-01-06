abf0cceae047b728ba2deefa4ea8996972e48845
pyeonne <pyeonne@gmail.com>
2022-01-06 17:46:52 +0900

    ⚡️  Show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-    return <h1>Hello :)</h1>;
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+        };
+
+        fetch(
+            'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDr9J2Z8_UFrAx4V0rZEHOHiQ_imeqJuN0&=AIzaSyDr9J2Z8_UFrAx4V0rZEHOHiQ_imeqJuN0',
+            requestOptions,
+        )
+            .then(response => response.json())
+            .then(result => setVideos(result.items))
+            .catch(error => console.log('error', error));
+    }, []);
+
+    return <VideoList videos={videos} />;
 }
 
 export default App;
